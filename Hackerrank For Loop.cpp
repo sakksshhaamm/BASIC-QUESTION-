da028f96ede99a51d57d31d3d790384ac2949191
Saksham Bhambota <55477878+sakksshhaamm@users.noreply.github.com>
2022-10-04 01:31:12 +0530

    Create Hackerrank For Loop.cpp

---
+++ Hackerrank For Loop.cpp
@@ -0,0 +1,42 @@
+1-https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem
+
+
+SOlUTION
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b,n;
+    cin>>a;
+    cin>>b;
+    cin>>n;
+    for (n=a;n<=b;n++)
+ {
+     if(n==1)
+     cout<<"one\n";
+     else if(n==2)
+     cout<<"two\n";
+     else if(n==3)
+     cout<<"three\n";
+     else if(n==4)
+     cout<<"four\n";
+     else if(n==5)
+     cout<<"five\n";
+     else if(n==6)
+     cout<<"six\n";
+     else if(n==7)
+     cout<<"seven\n";
+     else if(n==8)
+     cout<<"eight\n";
+     else if(n==9)
+     cout<<"nine\n";
+     else if(n>9 && n%2==0)
+     cout<<"even\n";
+     else
+     cout<<"odd\n";
+     
+ }
+    return 0;
+}
